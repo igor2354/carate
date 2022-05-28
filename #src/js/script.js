cbdc06c425742b41a5fd12c275bceb747f47f7bc
Igor Melnikov <igor2354@yandex.ru>
2022-05-28 11:52:00 +0300

    4

--- #src/js/script.js
+++ #src/js/script.js
@@ -4,37 +4,47 @@ $(document).ready(function () {
 		$("#menu > ul").append(clone);
 	});
 
+	$(".js-ham").on("click", function() {
+		$(this).toggleClass("active");
+		$(".mobile-menu").toggleClass("active");
+	});
+
+	$(".mobile-menu .js-anchor").on("click", function() {
+		$(".js-ham").removeClass("active");
+		$(".mobile-menu").removeClass("active");
+	});
+
 	// Активация мобильного меню
 
-	$("#menu").mmenu({
-		extensions: ["pagedim-black", "position-left"],
-		navbar: {
-			title: `<img src="${$(".header__logo img").attr("src")}">`,
-		},
-	});
+	// $("#menu").mmenu({
+	// 	extensions: ["pagedim-black", "position-left"],
+	// 	navbar: {
+	// 		title: `<img src="${$(".header__logo img").attr("src")}">`,
+	// 	},
+	// });
 
-	var $menu = $("#menu");
-	var $icon = $(".js-ham");
-	var API = $menu.data("mmenu");
+	// var $menu = $("#menu");
+	// var $icon = $(".js-ham");
+	// var API = $menu.data("mmenu");
 
-	function openMenu() {
-		API.open();
-	}
+	// function openMenu() {
+	// 	API.open();
+	// }
 
-	function closeMenu() {
-		API.close();
-	}
+	// function closeMenu() {
+	// 	API.close();
+	// }
 
-	API.bind("open:finish", function () {
-		$icon.addClass("active");
-		$("html").addClass("lock");
-	});
-	API.bind("close:finish", function () {
-		$icon.removeClass("active");
-		$("html").removeClass("lock");
-	});
+	// API.bind("open:finish", function () {
+	// 	$icon.addClass("active");
+	// 	$("html").addClass("lock");
+	// });
+	// API.bind("close:finish", function () {
+	// 	$icon.removeClass("active");
+	// 	$("html").removeClass("lock");
+	// });
 
-	$(".js-ham").on("click", openMenu);
+	// $(".js-ham").on("click", openMenu);
 
 	// ПЛАВНЫЙ ЯКОРЬ
 	$(".js-anchor").click(function () {
@@ -48,3 +58,78 @@ $(document).ready(function () {
 		return false;
 	});
 });
+
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+				// Открытие попапов МОЖНО УДАЛЯТЬ
+				let popupAllElem = Array.prototype.slice.call(document.querySelectorAll(".modal"));
+				let openButton = Array.prototype.slice.call(document.querySelectorAll(".js-modal-show"));
+				let closeButton = Array.prototype.slice.call(document.querySelectorAll(".js-modal-close"));
+				let body = document.querySelector("body");
+		
+				function openPopup(e) {
+					e.preventDefault();
+					let modal = document.querySelector(`#${e.target.dataset.popup}`);
+					modal.classList.add("active");
+		
+					body.classList.add("lock");
+		
+					setTimeout(() => {
+						modal.style.opacity = "1";
+					}, 100);
+		
+					if (e.target.classList.contains("--video")) {
+						let videoSrc = e.target.dataset.videoSrc;
+						let srcIframe = modal.querySelector("iframe").getAttribute("src");
+		
+						if (!srcIframe.includes(videoSrc)) {
+							modal.querySelector("iframe").setAttribute("src", srcIframe + videoSrc);
+						}
+					}
+				}
+		
+				function closePopup() {
+					popupAllElem.forEach((element) => {
+						if (element.classList.contains("active")) {
+							let modal = element;
+		
+							setTimeout(() => {
+								modal.classList.remove("active");
+							}, 300);
+		
+							modal.style.opacity = "0";
+		
+							body.classList.remove("lock");
+						}
+					});
+				}
+		
+				if (openButton != null) {
+					openButton.forEach((element) => {
+						element.addEventListener("click", (e) => {
+							closePopup(e);
+		
+							openPopup(e);
+						});
+					});
+				}
+		
+				if (closeButton != null) {
+					closeButton.forEach((element) => {
+						element.addEventListener("click", (e) => {
+							closePopup();
+						});
+					});
+				}
+		
+				if (popupAllElem != null) {
+					popupAllElem.forEach((element) => {
+						element.addEventListener("click", (e) => {
+							if (e.target.classList.contains("modal")) {
+								closePopup();
+							}
+						});
+					});
+				}
+	}, false);
